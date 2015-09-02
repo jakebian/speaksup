52d2c0652c8dfc8b7364e0dfa10103d39b2a06c4
Jake <findjakebian@gmail.com>
2015-09-02 15:59:11 -0400

    custom ghost config

--- launch.js
+++ launch.js
@@ -1,7 +1,33 @@
 'use strict';
 
-var ghost = require('ghost');
+/**
+ * Imports
+ */
 
-ghost().then(function (ghostServer) {
+var ghost = require('ghost');
+var path = require('path');
+
+
+/**
+ * Config
+ */
+
+var GhostConfig = {
+    config: path.join(__dirname, 'config.js')
+};
+
+
+/**
+ * Start Server
+ */
+
+ghost(GhostConfig).then(startGhost);
+
+
+/**
+ * Helpers
+ */
+
+function startGhost(ghostServer) {
     ghostServer.start();
-});
+}
